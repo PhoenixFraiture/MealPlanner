7f7e3eb976f1f5b7cd142715da924d1bdcaf7e51
phoenixfraiture <steak.and.hamz@gmail.com>
2024-02-28 11:50:25 -0500

    Initial Commit

---
+++ mealplanner/mealplannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealplannerApp.swift
+//  mealplanner
+//
+//  Created by 25pfraiture on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct mealplannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
