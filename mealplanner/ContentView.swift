a533b9f91a42ad23196da9a19a1e7917fb4989ec
phoenixfraiture <steak.and.hamz@gmail.com>
2024-07-18 12:59:48 -0400

    This is my Calendar

--- mealplanner/ContentView.swift
+++ mealplanner/ContentView.swift
@@ -1,24 +1,62 @@
-//
-//  ContentView.swift
-//  mealplanner
-//
-//  Created by 25pfraiture on 2/28/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var color: Color = .green
+    @State private var date = Date.now
+    @State private var onSecondScreen: Bool = false
+    
+        let daysOfWeek = Date.capitalizedFirstLettersOfWeekdays
+        let columns = Array(repeating: GridItem(.flexible()), count: 7)
+        @State private var days: [Date] = []
+        var body: some View {
+            VStack {
+                LabeledContent("Calendar Color") {
+                    ColorPicker("", selection: $color, supportsOpacity: false)
+                }
+                LabeledContent("Date/Time") {
+                    DatePicker("", selection: $date)
+                
+                }
+                HStack {
+                    ForEach(daysOfWeek.indices, id: \.self) { index in
+                    Text(daysOfWeek[index])
+                            .fontWeight(.black)
+                            .foregroundStyle(color)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                LazyVGrid(columns: columns) {
+                    ForEach(days, id: \.self) { day in
+                        if day.monthInt != date.monthInt {
+                            Text("")
+                        } else {
+                            Text(day.formatted(.dateTime.day()))
+                                .fontWeight(.bold)
+                                .foregroundStyle(.secondary)
+                                .frame( maxWidth: .infinity, minHeight: 50)
+                                .background(
+                                    Rectangle()
+                                        .foregroundStyle(
+                                            Date.now.startOfDay == day.startOfDay
+                                            ? .red.opacity(0.3)
+                                            :  color.opacity(0.3)
+                                        )
+                                )
+                        }
+                    }
+                }
+            }
+            .padding()
+            .onAppear {
+                days = date.calendarDisplayDays
+            }
+            .onChange(of: date) {
+                days = date.calendarDisplayDays
+            }
         }
-        .padding()
+        
     }
-}
 
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
